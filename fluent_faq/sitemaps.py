93a83de91259f780fa300ee5da4192ef735cdbcf
Diederik van der Boor <vdboor@edoburu.nl>
2014-09-08 11:27:08 +0200

    sitemap: avoid errors if FaqPage is not mounted

--- fluent_faq/sitemaps.py
+++ fluent_faq/sitemaps.py
@@ -1,6 +1,19 @@
 from django.contrib.sitemaps import Sitemap
+from django.core.urlresolvers import NoReverseMatch
 from fluent_faq.models import FaqCategory, FaqQuestion
 from fluent_faq.urlresolvers import faq_reverse
+
+
+def _url_patterns_installed():
+    # This module can use normal Django urls.py URLs, or mount the "FaqPage" in the page tree.
+    # Check whether the URLs are installed, so the `sitemap.xml` can be generated nevertheless.
+    # This issue will pop up elsewhere too, so there is no need to raise an error here.
+    try:
+        faq_reverse('faqcategory_detail', kwargs={'slug': 'category'}, ignore_multiple=True)
+    except NoReverseMatch:
+        return False
+    else:
+        return True
 
 
 class FaqQuestionSitemap(Sitemap):
@@ -8,6 +21,8 @@ class FaqQuestionSitemap(Sitemap):
     Sitemap for FAQ questions
     """
     def items(self):
+        if not _url_patterns_installed():
+            return None
         return FaqQuestion.objects.published()
 
     def lastmod(self, category):
@@ -25,6 +40,8 @@ class FaqCategorySitemap(Sitemap):
     Sitemap for FAQ categories.
     """
     def items(self):
+        if not _url_patterns_installed():
+            return None
         return FaqCategory.objects.published()
 
     def lastmod(self, category):
