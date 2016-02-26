4f71339cad35b2444ea295fd4b518e539f1088bb
Diederik van der Boor <vdboor@edoburu.nl>
2016-02-26 16:21:58 +0100

    Fix Django 1.9 warnings about patterns('', ..)

--- fluent_faq/urls.py
+++ fluent_faq/urls.py
@@ -1,8 +1,8 @@
-from django.conf.urls import patterns, url
+from django.conf.urls import url
 from .views import FaqQuestionList, FaqCategoryDetail, FaqQuestionDetail
 
-urlpatterns = patterns('',
+urlpatterns = [
     url(r'^$', FaqQuestionList.as_view(), name='faqquestion_index'),
     url(r'^(?P<slug>[^/]+)/$', FaqCategoryDetail.as_view(), name='faqcategory_detail'),
     url(r'^(?P<cat_slug>[^/]+)/(?P<slug>[^/]+)/$', FaqQuestionDetail.as_view(), name='faqquestion_detail'),
-)
+]
